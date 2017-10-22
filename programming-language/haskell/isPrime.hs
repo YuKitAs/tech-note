2411487d9840f1c01f2788031ad4c753586124c2
YuKitAs <moncher.merci@gmail.com>
2017-10-22 17:06:19 +0200

    Modify a note

--- programming-language/haskell/isPrime.hs
+++ programming-language/haskell/isPrime.hs
@@ -1,6 +1,5 @@
 -- Determine if a natural number is prime.
 
 isPrime :: Int -> Bool
-isPrime x
-    | x == 1 = True
-    | otherwise = null [k | k <- [2..isqrt x], x `mod` k == 0] where isqrt = floor . sqrt . fromIntegral
+isPrime x = if x == 1 then True else null [k | k <- [2..isqrt x], x `mod` k == 0]
+    where isqrt = floor . sqrt . fromIntegral

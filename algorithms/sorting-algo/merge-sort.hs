91626b88f41b09537147d46bb63e7371c0ce44d1
YuKitAs <moncher.merci@gmail.com>
2017-10-30 16:48:56 +0100

    Modify handling for empty list

--- algorithms/sorting-algo/merge-sort.hs
+++ algorithms/sorting-algo/merge-sort.hs
@@ -5,6 +5,6 @@ mergeSort [] = []
 mergeSort [x] = [x]
 mergeSort xs = let (a, b) = splitAt ((length xs) `div` 2) xs
                in merge (mergeSort a) (mergeSort b)
-               where merge [] (y:ys) = (y:ys)
-                     merge (x:xs) [] = (x:xs)
+               where merge [] ys = ys
+                     merge xs [] = xs
                      merge (x:xs) (y:ys) = if x <= y then x:(merge xs (y:ys)) else y:(merge ys (x:xs))

5a1cadfc29630cb12fa92510b2a6d13241dfad1a
YuKitAs <moncher.merci@gmail.com>
2017-10-30 16:53:15 +0100

    Update insertion-sort.hs

--- algorithms/sorting-algo/insertion-sort.hs
+++ algorithms/sorting-algo/insertion-sort.hs
@@ -5,3 +5,10 @@ insertSort [] = []
 insertSort (x:xs) = insert x (insertSort xs)
                     where insert y [] = [y]
                           insert y (x:xs) = if y > x then x:(insert y xs) else y:x:xs
+
+-- An alternative with foldr
+insertSort :: [Int] -> [Int]
+insertSort [] = []
+insertSort xs = foldr insert [] xs
+                where insert y [] = [y]
+                      insert y (x:xs) = if y > x then x:(insert y xs) else y:x:xs

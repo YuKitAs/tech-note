2587e852545629883b4ddcc932a161e70b48343d
YuKitAs <moncher.merci@gmail.com>
2019-11-04 22:22:02 +0100

    Add comments

--- algorithm/sorting-algo/Quicksort.java
+++ algorithm/sorting-algo/Quicksort.java
@@ -1,12 +1,20 @@
+/**
+ * Quicksort in Java
+ */
 public class Quicksort {
     public void sort(int[] arr, int l, int r) {
         if (l < r) {
+            // calculate the partition position and sort left/right partition recursively
             int par = partition(arr, l, r);
             sort(arr, l, par - 1);
             sort(arr, par + 1, r);
         }
     }
 
+    /**
+     * Swap the smaller elements to the left of the pivot and the greater elements to the right
+     * Return the final position of the pivot as partition position
+     */
     private int partition(int[] arr, int l, int r) {
         int p = arr[r]; // select the last element as pivot
         int i = l; // left pointer
@@ -27,7 +35,7 @@ public class Quicksort {
             }
 
             if (i >= j) {
-                // swap pivot to i
+                // swap pivot to the correct position
                 int temp = arr[i];
                 arr[i] = p;
                 arr[r] = temp;
